1bc80f8c6ed1eb00d12ebd5c8aa1c00807e4d3bb
Carlos Nassif <costa.carlos12@gmail.com>
2023-09-12 21:02:59 -0300

    exemplo aula

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ console.log('Soma 2 e 3:', calc.somar(2, 3));
 console.log('Divide 10 e 2:', calc.dividir(10, 2));
 console.log('Multiplica 10 e 2:', calc.multiplicar(10, 2));
 console.log('Subtrai 10 e 2:', calc.subtrair(10, 2));
+console.log('Subtrai 10 e 5:', calc.subtrair(10, 5));
